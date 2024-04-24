8d9c8cf031bdce99f4ed68959b9ba364a9fae0d1
GannuAbhijithsai <96736183+GannuAbhijithsai@users.noreply.github.com>
2024-04-24 18:57:49 +0530

    Update Home.js

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -53,7 +53,7 @@ export default function Home() {
   const handleResetClick = () => {
     setStartTime(0);
     setElapsedTime(0);
-   
+    setIsRunning(false);
     // Reset localStorage value
     localStorage.removeItem('time');
   };
